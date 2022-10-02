7400a2c2a44fce5fe813f4543c25744d9ab1e1ed
Ibrat Zayniddinov <tony@Ibrats-Mac-mini.local>
2022-10-02 21:29:40 +0500

    ask time function is added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from oxforddict import getDefinitions
 from googletrans import Translator
+from datetime import date, datetime
+
 API_TOKEN = '5765266596:AAEUcs6KxXbfYYf4CaicI0B65wB2pAom_18'
 
 translator = Translator()
@@ -21,6 +23,12 @@ async def send_welcome(message: types.Message):
 async def ask_help(message: types.Message):
     await message.answer('What can I help you, my friend!')
 
+@dp.message_handler(commands=['time'])
+async def ask_time(message: types.Message):
+    now = datetime.now()
+    date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+    await message.answer(date_time.title())
+
 @dp.message_handler()
 async def interpreter(message: types.Message):
     lang = translator.detect(message.text).lang
@@ -31,6 +39,9 @@ async def interpreter(message: types.Message):
     if len(message.text.split()) < 2:
         dest2 = 'ru' if lang2 == 'en' else 'en'
         await message.reply(translator.translate(message.text, dest2).text)
+
+
+
 
 
 if __name__ == '__main__':
